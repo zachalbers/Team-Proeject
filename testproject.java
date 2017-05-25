c36195b592b9c65c4a02027dcd2e764fa3de25b9
usmankcpsc <u.khawar21@gmail.com>
2017-05-25 11:07:06 -0600

    bug fix
    on test project. works on mac now!

--- testproject.java
+++ testproject.java
@@ -1,38 +1,40 @@
-package testproject;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
+
+@SuppressWarnings("rawtypes")
 public class testproject {
-	
-	
-	
+
+
+
 public static void gui()  {
-	
+
 		// transport combo box
 		String [] transportmethod = {"Walk","Bike","Skateboard"};
-		JComboBox transport = new JComboBox(transportmethod);
+		JComboBox<String> transport = new JComboBox<>(transportmethod);
 		transport.setLayout(null);
 		transport.setBounds(20,100,100,35);
-		
-		//Making a combo box 
+
+		//Making a combo box
 		String [] buildings = {"Mac Hall","Science B","Kinesiology A","Kinesiology B","Hotel Alma","MacEwan Student centre"};
-		JComboBox destination = new JComboBox(buildings);
+		JComboBox<String> destination = new JComboBox<>(buildings);
 		destination.setLayout(null);
-		JComboBox currentloc = new JComboBox(buildings);
+		JComboBox<String> currentloc = new JComboBox<>(buildings);
 		destination.setLayout(null);
 		// Creates the main window of the gui
 		JFrame window = new JFrame("UniMAP 2.0 BETA");   // "TeST PROGRAM" is Title of window
 		window.setVisible(true);     // makes window visible
 		window.setSize(500,500);     // set the dimensions of window
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // So that gui closes when user clicks cross
-		
+
 		JPanel panels = new JPanel(); // Makes new panel
 		panels.setBackground(Color.WHITE); // background colour of panel
 		panels.setLayout(null); // enables us to put components anywhere in panel
-		
-		
-		
+
+
+
 		JLabel title = new JLabel("UniMAP 2.0 Beta");
 
 		JButton exit = new JButton("Exit");   //Creates new button
@@ -54,7 +56,7 @@ public static void gui()  {
 		panels.add(destination);
 		panels.add(title);
 		title.setForeground(Color.red); // Sets colour of the label
-		
+
 		JLabel walktime = new JLabel("Destination:");
 		walktime.setLayout(null);
 		walktime.setBounds(250,200,120,30);
@@ -67,14 +69,14 @@ public static void gui()  {
 		panels.add(walktime2);
 		panels.add(transport);
 		window.add(panels); // Adds panel to the window
-		
-		
-		
-		
+
+
+
+
 		// ACTION LISTENERS
-		
-	
-		
+
+
+
 		// EXIT BUTTON
 		exit.addActionListener(new ActionListener() {
 		    public void actionPerformed(ActionEvent e)
@@ -82,18 +84,18 @@ public static void gui()  {
 		    	System.exit(0);
 		    }
 		});
-		
-		
+
+
 	// DROPDOWN CURRENT LOCATION
 		currentloc.addActionListener(new ActionListener() {
 		    public void actionPerformed(ActionEvent e)
 		    {
 		    	String startpoint = (String) currentloc.getSelectedItem();
-		    	
-		    	
+
+
 		    }
 		});
-		
+
 	// GO BUTTON
 		start.addActionListener(new ActionListener() {
 		    public void actionPerformed(ActionEvent e)
@@ -103,21 +105,21 @@ public static void gui()  {
 				  JOptionPane.showMessageDialog(null, infoMessage,titleBar, JOptionPane.INFORMATION_MESSAGE);
 		    }
 		});
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
+
+
+
+
+
+
+
+
+
 
 }
 	public static void main(String[] args) {
 		gui();
-		
+
 	}
 
 }
