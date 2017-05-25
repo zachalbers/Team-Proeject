d9b7ddafdea3d451a11db0f1baa3840fc14ad55d
TJKR21 <tjk.randall@gmail.com>
2017-05-25 13:32:45 -0600

    coord

--- AllBuildings.java
+++ AllBuildings.java
@@ -14,37 +14,37 @@ public class AllBuildings {
 
                 // Creating all the buildings
                 Building Science_A = new Building();
-                Science_A.setCoordinates(0, 0);
+                Science_A.setCoordinates(1282, 799 );
                 Science_A.setBuildingName("Science A");
                 Science_A.setConnections("Science B");
 
                 Building Science_B = new Building();
-                Science_B.setCoordinates(10, 0);
+                Science_B.setCoordinates(1295, 794);
                 Science_B.setBuildingName("Science B");
                 Science_B.setConnections("Mac Hall,Science A");
 
                 Building Mac_Hall = new Building();
-                Mac_Hall.setCoordinates(20, 0);
+                Mac_Hall.setCoordinates(1305, 786 );
                 Mac_Hall.setBuildingName("Mac Hall");
                 Mac_Hall.setConnections("MacEwan Student Centre,Science B");
 
                 Building MacEwan_Student_Centre = new Building();
-                MacEwan_Student_Centre.setCoordinates(30, 0);
+                MacEwan_Student_Centre.setCoordinates(1318, 782);
                 MacEwan_Student_Centre.setBuildingName("MacEwan Student Centre");
                 MacEwan_Student_Centre.setConnections("Mac Hall,Kinesiology B");
 
                 Building Kinesiology_B = new Building();
-                Kinesiology_B.setCoordinates(40, 0);
+                Kinesiology_B.setCoordinates(1337, 779);
                 Kinesiology_B.setBuildingName("Kinesiology B");
                 Kinesiology_B.setConnections("MacEwan Student Centre,Kinesiology A");
 
                 Building Kinesiology_A = new Building();
-                Kinesiology_A.setCoordinates(50, 0);
+                Kinesiology_A.setCoordinates(1331, 772);
                 Kinesiology_A.setBuildingName("Kinesiology A");
                 Kinesiology_A.setConnections("Kinesiology B,Hotel Alma");
 
                 Building Hotel_Alma = new Building();
-                Hotel_Alma.setCoordinates(60, 0);
+                Hotel_Alma.setCoordinates(1329, 761);
                 Hotel_Alma.setBuildingName("Hotel Alma");
                 Hotel_Alma.setConnections("Kinesiology A");
 
