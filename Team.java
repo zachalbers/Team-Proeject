76c9f3d305a981a0ec0e8dd0a145a18f6a198566
zachalbers <back2zach2@gmail.com>
2017-05-31 11:04:45 -0600

    Deleted comments

--- Team.java
+++ Team.java
@@ -4,67 +4,16 @@ import java.lang.Object;
 import java.util.*;
 
 // This is the main class that is run.
-// Currently it also contains the pathfinding alogrithm and the distance calculator.
 
 public class Team {
 
+    public void start() {
+        GUI userInfo = new GUI();
+        userInfo.gui();
+    }
 
-
-
-
-public void start() {
-  // // Calls GUI class
-  // ComboBoxExample UserGUI = new ComboBoxExample();
-  // UserGUI.main(args);
-
-
-  GUI userInfo = new GUI();
-  userInfo.gui();
-
-
-
-
-  // // Getting User input
-  // Travel nextTravel = new Travel();
-  // String location = nextTravel.getLocation();
-  // String destination = nextTravel.getDestination();
-
-
-
-
-
-
-
-
-
-}
-
-
-// public void execute(){
-//
-//   AllBuildings nextBuildingObj = new AllBuildings();
-//   Building locationObj = nextBuildingObj.buildings(location);
-//   Building destinationObj = nextBuildingObj.buildings(destination);
-//
-//
-//
-//   Pathfinder finder = new Pathfinder();
-//   finder.pathfind(locationObj, destinationObj, nextBuildingObj);
-//
-//
-// }
-
-
-
-
-
-
-
-public static void main(String[] args) {
+    public static void main(String[] args) {
         Team main = new Team();
         main.start();
-                }
-
-
-
+                    }
 }
