db42e87e5822554fb1392c4c2061c8ad20d73399
TJKR21 <tjk.randall@gmail.com>
2017-06-01 12:49:30 -0600

    height width abbriviations

--- Building.java
+++ Building.java
@@ -5,42 +5,69 @@ import java.util.Arrays;
 
 public class Building {
 
+    int width = 0;
+    int hight = 0;
     double xCoord = 0;
     double yCoord = 0;
     String buildingName = "";
     String[] connections = {};
 
+// sets x,y for building location on map
     public void setCoordinates(double x, double y) {
         xCoord = x;
         yCoord = y;
     }
-
+// return x coordinate of building
     public double getX() {
         return xCoord;
     }
-
+// return y coordinate of building
     public double getY() {
         return yCoord;
     }
-
+// Give building obj a name to be referanced for input
     public void setBuildingName(String name) {
         buildingName = name;
     }
-
+// return name of building
     public String getBuildingName() {
         return buildingName;
     }
 
-
+// sets building connections for pathfinder
     public void setConnections(String newConnections) {
         connections = newConnections.split(",");
     }
 
-
+// return connections for pathfinder
     public String[] getConnections() {
         return connections;
+    }
 
+// return Building hight for map
+    public int getHeight(){
+        return height;
+    }
 
+// sets Height for map
+    public void setHeight(int h){
+        height=h;
+    }
+// return Building width for map
+    public int getWidth(){
+        return width;
+    }
+// sets width for map
+    public void setWidth(int w){
+        width=w;
+    }
+// return abbriviation for building
+    public String getAbbreviation(){
+        return abbreviation;
+    }
+// sets abbriviations for building objects
+    public void setAbbreviation(String abbr){
+      abbreviation = abbr;
     }
 
 }
