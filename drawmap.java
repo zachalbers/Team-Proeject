bada8ac305d06709f5d842c70f10f75db9871fee
zachalbers <back2zach2@gmail.com>
2017-06-02 10:52:54 -0600

    change

--- drawmap.java
+++ drawmap.java
@@ -1,16 +1,20 @@
-// imports required libraries
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-
-
-import java.util.ArrayList;
-
 import java.lang.Object;
 import java.util.*;
 
 // Class for making the map   ++++++++++++
 public class drawmap extends JPanel{
+
+
+
+	java.util.List<String> pathList = Collections.synchronizedList(new ArrayList<String>());
+
+
+	public drawmap(java.util.List<String> pathList) {
+	this.pathList = pathList;
+	}
 
 	// method that draws the buildings and its names  ++++++++++++++++
 	public void paintComponent(Graphics g){
@@ -18,11 +22,8 @@ public class drawmap extends JPanel{
 		this.setBackground(Color.YELLOW);
 
 		AllBuildings map1= new AllBuildings();
-		Pathfinder path1 = new Pathfinder();
 
 
-  // List<String> pathList2 = Collections.synchronizedList(new ArrayList<String>());
-	// pathList2 = path1.pathList;
 
 
 
@@ -46,34 +47,23 @@ public class drawmap extends JPanel{
 				g.fillRect(xcoord,ycoord, length*7, width*7);
 		}
 
-				System.out.println(path1.pathList.size());
 
 
-			for (int i = 0; i<(path1.pathList.size() - 1);i++ ) {
-								int x1 = (int)map1.buildings(path1.pathList.get(i)).getX();
-								int x2 = (int)map1.buildings(path1.pathList.get(i+1)).getX();
-								int y1 = (int)map1.buildings(path1.pathList.get(i)).getY();
-								int y2 = (int)map1.buildings(path1.pathList.get(i+1)).getY();
-								g.drawLine(x1*10,(800-y1*10),x2*10,(800-y2*10));
-								// System.out.println("getting");
+
+				for (int i = 0; i<(pathList.size() - 1);i++ ) {
+									int x1 = (int)map1.buildings(pathList.get(i)).getX();
+									int x2 = (int)map1.buildings(pathList.get(i+1)).getX();
+									int y1 = (int)map1.buildings(pathList.get(i)).getY();
+									int y2 = (int)map1.buildings(pathList.get(i+1)).getY();
+									g.drawLine(x1*10,(800-y1*10),x2*10,(800-y2*10));
 
 
-			}
 
-
+				}
 
 
 
 
-    // Science B
-		// g.setColor(Color.BLUE);
-		// g.fillRect(map1.getX(),map1.getY(), 3, 3);
-		//
-		// g.setColor(Color.BLACK);
-		// g.fillRect(1005, 140, 100, 30);
-		//
-		// g.setColor(Color.RED);
-		// g.drawString("RECTANGLES", 25, 120);
 
 	}
 
